1d35fa59d54eca8e46ef43d2b3ee42f9b3c0e77b
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-01-11 00:03:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-      API_ID = int(os.environ.get("APP_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
+      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6885232721:AAGkYm_7SW1IUdwJYTJ8eZwC4PX39rU3_uk")
+      API_ID = int(os.environ.get("APP_ID", 29942004))
+      API_HASH = os.environ.get("API_HASH", "ad92f01e4a90cddebbea0ad16fa23026")
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
-      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "Ts_Bots")
-      ADMIN_ID = int(os.environ.get("ADMIN_ID", 123476535 )) 
-      DB_URL = os.environ.get("DATABASE_URL", "")
+      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "Punikin")
+      ADMIN_ID = int(os.environ.get("ADMIN_ID", 1680073194 )) 
+      DB_URL = os.environ.get("DATABASE_URL", "mongodb+srv://jnanesh:jnanesh@cluster0.8pzxa6s.mongodb.net/?retryWrites=true&w=majority")
